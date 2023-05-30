91ff342eb49d62e3509434ebadf7b93c73c0203c
Jraaf <nasibovrafael1@gmail.com>
2023-05-30 16:56:35 +0300

    other tempalte

---
+++ FilmReviewBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace FilmReviewBot
+{
+    internal class Program
+    {
+        private static TelegramBotClient botClient;
+        static void Main(string[] args)
+        {
+            botClient = new TelegramBotClient("6095290532:AAG7Ask50jYtMXkR3mOkTdBxvHKAjZs9rWM");
+            botClient.
+        }
+    }
+}
